17b076804cefbedbeeaafe5543ec324965f82cb9
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-05-01 08:40:51 -0400

    Moved files

--- Package.swift
+++ Package.swift
@@ -16,19 +16,6 @@ let package = Package(
         .executableTarget(
             name: "NoLifeWzToNxTool",
             dependencies: [
-                "CNoLifeWzToNx",
-                "CLZ4",
-                "CSquish"
-            ],
-            linkerSettings: [
-                .linkedLibrary("z"),
-                .linkedLibrary("lz4"),
-                .linkedLibrary("squish")
-            ]
-        ),
-        .target(
-            name: "CNoLifeWzToNx",
-            dependencies: [
                 "CLZ4",
                 "CSquish"
             ],
@@ -37,6 +24,15 @@ let package = Package(
                 .unsafeFlags([
                     "-I", "/opt/homebrew/Cellar/lz4/1.9.4/include",
                     "-I", "/opt/homebrew/Cellar/libsquish/1.15_1/include",
+                ], .when(platforms: [.macOS]))
+            ],
+            linkerSettings: [
+                .linkedLibrary("z"),
+                .linkedLibrary("lz4"),
+                .linkedLibrary("squish"),
+                .unsafeFlags([
+                    "-L/opt/homebrew/Cellar/lz4/1.9.4/lib",
+                    "-L/opt/homebrew/Cellar/libsquish/1.15_1/lib"
                 ], .when(platforms: [.macOS]))
             ]
         ),
